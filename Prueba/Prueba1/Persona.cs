953f53fb440839d25ea0dbefdf3d73526c30d099
Renzoh17 <hirschfeldrenzo@gmail.com>
2024-08-01 16:57:47 -0300

    Creación
    Se creo el repositorio
    Se creo una solución de prueba

---
+++ Prueba/Prueba1/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1
+{
+    internal class Persona
+    {
+        //VARIABLES
+        //Primera Manera Encapsulamiento
+        public string nombre { get; private set; }
+        public string apellido { get; private set; }
+        //Segunda manera Encapsulamiento
+        private int dni;
+        public int Dni
+        {
+            get
+            {
+                return dni;
+            }
+            private set
+            {
+                dni = value;
+            }
+        } 
+         
+        //METODOS
+        public Persona(string nombre, string apellido, int dni){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+        }
+
+
+    }
+}
---
+++ Prueba/Prueba1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Renzo","Hirschfeld", 43756001);
+            Console.WriteLine("Nombre: {0}\nApellido: {1}\nDNI: {2}",p1.nombre,p1.apellido,p1.Dni);
+
+        }
+    }
+}
